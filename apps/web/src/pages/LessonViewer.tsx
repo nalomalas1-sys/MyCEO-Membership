5a291cef4d249292486dca2b389d5406f67bb6a1
Macchiin <143122622+Macchiin@users.noreply.github.com>
2026-01-02 19:25:43 +0800

    Fix error

--- apps/web/src/pages/LessonViewer.tsx
+++ apps/web/src/pages/LessonViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ChildNavBar } from '@/components/navigation/ChildNavBar';
 import { LoadingAnimation } from '@/components/ui/LoadingAnimation';
